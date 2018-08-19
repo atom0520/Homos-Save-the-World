d50574f7740e69144bcd12eda0fb7cdaa2ee16f6
atom0520 <atom0520@hotmail.com>
2018-08-18 23:16:22 -0600

    first commit

---
+++ Source/LD42/LD42.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LD42.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LD42, "LD42" );
---
+++ Source/LD42/LD42GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LD42GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LD42_API ALD42GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
